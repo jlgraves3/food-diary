fad88e4eccefc5986f05fb04c620e8479325ff20
Julia Graves <jgraves@wesleyan.edu>
2017-07-31 14:53:08 -0400

    added code to food-routes.js

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const app = express();
+require('dotenv').config();
+app.use(methodOverride('_method'))
+
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
---
+++ routes/food-routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const foodRoutes = express.Router();
+
+const foodController = require('../controllers/food-entry-controller');
+
+foodRoutes.get('/',foodController.index);
+foodRoutes.post('/',foodController.create);
+
+foodRoutes.get('/add', (req,res) => {
+	res.send({
+		message: add
+	});
+});
+
+foodRoutes.get('/:id',foodController.show);
+foodRoutes.put('/:id',foodController.update);
+foodRoutes.delete('/:id',foodController.delete);
+
+module.exports = foodRoutes;
