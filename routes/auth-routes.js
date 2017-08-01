9894f50cad864e00fb721fff80862e161dc6c043
Julia Graves <jgraves@wesleyan.edu>
2017-08-01 11:50:55 -0400

    added routes to auth-routes.js

---
+++ routes/auth-routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+const usersController = require('../controllers/users-controller');
+
+authRouter.get('/login', authHelpers.loginRedirect, (req,res) => {
+	res.render('auth/login', {});
+});
+
+authRouter.get('/register',authHelpers.loginRedirect, (req,res) => {
+	res.render('auth/register', {});
+});
