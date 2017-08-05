7b629739e3d63f0f7707d1962a7e0b3d433de202
Julia Graves <jgraves@wesleyan.edu>
2017-08-05 15:53:30 -0400

    commented helper files

--- services/auth/auth-helpers.js
+++ services/auth/auth-helpers.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
 
+//compate passwords
 function comparePass(userPassword, databasePassword) {
 	return bcrypt.compareSync(userPassword, databasePassword);
 }
-
+//redirect user profile if logged in
 function loginRedirect(req,res,next) {
 	if (req.user) return res.redirect('/user');
 	return next();
 }
 
+//require log in - redirect to login page if not logged in
 function loginRequired(req,res,next) {
 	if (!req.user) return res.redirect('/auth/login');
 	return next();
--- services/food-helper.js
+++ services/food-helper.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 const APP_ID = process.env.APP_ID;
 
+//get search results from nutritionix API from search query
 function getItemsFromApi(req,res,next) {
-	console.log('get items from api');
+	//search item
 	let itemName = req.params.item;
-	console.log('Hello world!');
-	console.log(req.params.item);
+	//API request
 	fetch(`https://api.nutritionix.com/v1_1/search/${req.params.item}?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=${APP_ID}&appKey=${API_KEY}`)
 	.then(fetchRes => fetchRes.json())
 	.then(jsonRes => {
